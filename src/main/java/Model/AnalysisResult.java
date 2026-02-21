866bb6f67b41450f5a768793698b03c7f4264006
vythili6 <yo.vythilingum@mail.utoronto.ca>
2026-02-21 16:39:56 -0500

    Project setup + analysis result implemented

---
+++ src/main/java/Model/AnalysisResult.java
@@ -0,0 +1,101 @@
+package Model;
+
+import java.util.List;
+import java.util.Map;
+
+public class AnalysisResult {
+
+    // Basic Counts
+    private final int wordCount;
+    private final int charCountWithSpaces;
+    private final int charCountWithoutSpaces;
+    private final int paragraphCount;
+    private final int sequenceCount;
+    private final int uniqueWordCount;
+
+    // Derived Stats
+    private final double averageWordLength;
+    private final double averageSentenceLength; // average number of words per sentence
+    private final double readingTimeSeconds; // compute from word count / reading speed (200 wpm)
+    private final double fleschReadingEase; // readability score computed in the analyzer
+
+    // charts data
+    private final Map<String, Integer> wordFrequency; //maps each word to how many times it appears
+    private final Map<Character, Integer> charFrequency; //maps each letter to its a-z count
+    private final List<Integer> sentenceLengths; // a list where each element is the word count of one sentence
+
+
+    public AnalysisResult(int wordCount, int charCountWithSpaces, int charCountWithoutSpaces, int paragraphCount,
+                          int sequenceCount, int uniqueWordCount, double averageWordLength, double averageSentenceLength,
+                          double readingTimeSeconds, double fleschReadingEase,
+                          Map<String, Integer> wordFrequency, Map<Character, Integer> charFrequency,
+                          List<Integer> sentenceLengths) {
+        this.wordCount = wordCount;
+        this.charCountWithSpaces = charCountWithSpaces;
+        this.charCountWithoutSpaces = charCountWithoutSpaces;
+        this.paragraphCount = paragraphCount;
+        this.sequenceCount = sequenceCount;
+        this.uniqueWordCount = uniqueWordCount;
+        this.averageWordLength = averageWordLength;
+        this.averageSentenceLength = averageSentenceLength;
+        this.readingTimeSeconds = readingTimeSeconds;
+        this.fleschReadingEase = fleschReadingEase;
+        this.wordFrequency = wordFrequency;
+        this.charFrequency = charFrequency;
+        this.sentenceLengths = sentenceLengths;
+    }
+
+    // Getters
+    public int getWordCount(){
+        return wordCount;
+    }
+
+    public int getCharCountWithSpaces(){
+        return charCountWithSpaces;
+    }
+
+    public int getCharCountWithoutSpaces(){
+        return charCountWithoutSpaces;
+    }
+
+    public int getParagraphCount(){
+        return paragraphCount;
+    }
+
+    public int getSequenceCount(){
+        return sequenceCount;
+    }
+
+    public int getUniqueWordCount(){
+        return uniqueWordCount;
+    }
+
+    public double getAverageWordLength(){
+        return averageWordLength;
+    }
+
+    public double getAverageSentenceLength(){
+        return averageSentenceLength;
+    }
+
+    public double getReadingTimeSeconds(){
+        return readingTimeSeconds;
+    }
+
+    public double getFleschReadingEase(){
+        return fleschReadingEase;
+    }
+
+    public Map<String, Integer> getWordFrequency(){
+        return wordFrequency;
+    }
+
+    public Map<Character, Integer> getCharFrequency(){
+        return charFrequency;
+    }
+
+    public List<Integer> getSentenceLengths(){
+        return sentenceLengths;
+    }
+
+}
